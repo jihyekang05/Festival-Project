7a57ce4da2a32a9e5502d077081659c86174b7c4
jihyekang05 <jirae050303@gmail.com>
2022-11-09 09:19:22 +0900

    jihye 2022-11-09

--- src/main/java/com/festivalP/demo/controller/ChattingController.java
+++ src/main/java/com/festivalP/demo/controller/ChattingController.java
@@ -1,4 +1,15 @@
 package com.festivalP.demo.controller;
 
-public class ChattingController {
+import com.festivalP.demo.domain.review;
+import com.festivalP.demo.service.FestivalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ReviewController {
+
+
 }
---
+++ src/main/java/com/festivalP/demo/repository/ReviewRepository.java
@@ -0,0 +1,2 @@
+package com.festivalP.demo.repository;public class ReviewRepository {
+}
